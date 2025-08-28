0cb37f85deb4ab3771821381fc5eab5385b0b160
minuezz <m.aksneverov2005@gmail.com>
2025-08-28 18:55:09 +0200

    feat: added AppUser entity, UserRole enum

---
+++ QLine.Domain/Enums/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLine.Domain.Enums
+{
+    public enum UserRole
+    {
+        Client = 0,
+        Staff = 1,
+        Admin = 2
+    }
+}
