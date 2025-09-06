e098d3f24dbedc2fac9db75d3e6c4d58151e3cfd
minuezz <m.aksneverov2005@gmail.com>
2025-09-07 00:11:06 +0200

    fix: QLine.Web interactivity bug fixed

--- QLine.Web/Program.cs
+++ QLine.Web/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using QLine.Domain.Entities;
 using QLine.Infrastructure;
@@ -14,29 +14,11 @@ using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Host.UseSerilog((ctx, lc) => lc
-//    .ReadFrom.Configuration(ctx.Configuration)
-//    .Enrich.FromLogContext()
-//    .WriteTo.Console());
-
 builder.Services.AddInfrastructure(builder.Configuration);
-
 builder.Services.AddApplication();
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
-//builder.Services.AddDbContext<AppDbContext>(opt =>
-//    opt.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
-
-//builder.Services.AddAuthentication().AddIdentityCookies();
-//builder.Services.AddAuthorization();
-//builder.Services
-//    .AddIdentityCore<AppUser>(o => { /* пароли/требования */ })
-//    .AddEntityFrameworkStores<AppDbContext>();
-
-//builder.Services.AddSignalR();
 
 //builder.Services.AddMudServices();
 
@@ -48,30 +30,18 @@ using (var scope = app.Services.CreateScope())
     await DbInitializer.InitializeAsync(db);
 }
 
-// Configure the HTTP request pipeline.
+// Configure pipeline
 if (!app.Environment.IsDevelopment())
 {
-    //app.UseDeveloperExceptionPage();
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
-    //using var scope = app.Services.CreateScope();
-    //var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    //await db.Database.MigrateAsync();
-    //await DbSeed.SeedAsync(db);
 }
 
-//app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-//app.UseAuthentication();
-//app.UseAuthorization();
-
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
-//app.MapHub<QueueHub>("/hubs/queue");
 
 app.Run();
