17f09670fe206ad393808301f9e9a1ebbfe7671c
minuezz <68457871+minuezz@users.noreply.github.com>
2025-12-20 16:42:12 +0100

    Bind reservations to current user

--- QLine.Application/Features/Reservations/Commands/CreateReservationCommand.cs
+++ QLine.Application/Features/Reservations/Commands/CreateReservationCommand.cs
@@ -1,17 +1,11 @@
-﻿using QLine.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MediatR;
 using QLine.Application.DTO;
+
 namespace QLine.Application.Features.Reservations.Commands
 {
     public sealed record CreateReservationCommand(
         Guid ServicePointId,
         Guid ServiceId,
-        Guid UserId,
         DateTimeOffset StartTime
         ) : IRequest<ReservationDto>;
 
